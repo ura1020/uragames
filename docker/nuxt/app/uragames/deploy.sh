bbbedc46c6704085c879bae943f720b0bc543ce8
ura1020 <ura1020@gmail.com>
2018-08-19 15:35:38 +0900

    nuxt resources
    .gitignore

--- docker/nuxt/app/uragames/deploy.sh
+++ docker/nuxt/app/uragames/deploy.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-npm run generate
+# "awscli" run outside container.
+
+aws s3api create-bucket \
+  --bucket uragames \
+  --region ap-northeast-1 \
+  --create-bucket-configuration LocationConstraint=ap-northeast-1
+
+aws s3 website s3://uragames/ \
+  --index-document index.html \
+  --error-document index.html
+
+aws s3api put-bucket-policy \
+  --bucket uragames \
+  --policy file://policy.json
+
+aws s3 cp dist s3://uragames/ \
+  --recursive
